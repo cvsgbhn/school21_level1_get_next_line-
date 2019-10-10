9cfe761ff361ffb731345fb1714aed0a09f01ea6
cvsgbhn <kbh123456g@gmail.com>
2019-10-10 19:05:03 +0300

    Yep, Steven was right on this one.

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,16 @@
 /*   By: vdanilo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 18:18:13 by vdanilo           #+#    #+#             */
-/*   Updated: 2019/10/09 18:29:24 by vdanilo          ###   ########.fr       */
+/*   Updated: 2019/10/10 18:03:34 by vdanilo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+//char	*check_fd(char **line, char *ending)
+//{
+
+//}
 
 char		*check_ending(char **line, char *ending)
 {
@@ -44,7 +49,10 @@ int		get_next_line(const int fd, char **line)
 	char		*temp_array;
 	int			num_read;
 
-	if (fd < 0 || fd > 256 || !line)
+	//printf("%d\n", fd);
+	if (fd < 0 || fd > 256)
+		return (-1);
+	if (read(fd, NULL, 0) == -1)
 		return (-1);
 	ptr_to_end = check_ending(line, ending);
 	while (!ptr_to_end && (num_read = read(fd, buf, BUFF_SIZE)))
@@ -66,7 +74,8 @@ int		get_next_line(const int fd, char **line)
 	//	printf("%d\n", 0);
 	if (!ft_strlen(*line))
 	{
-		return ((ft_strlen(ending) || num_read) ? 1 : 0);
+		return ((!num_read || !(ft_strlen(ending))) ? 0 : 1);
+		//return ((ft_strlen(ending) || num_read) ? 1 : 0);
 	}
 	else
 		return (1);
--- main.c
+++ main.c
@@ -6,12 +6,25 @@ int		main(void)
 	int 	fd;
 	int		a;
 
-	fd = open("gnl7_1.txt", O_RDONLY);
-	while ((a = get_next_line(fd, &line)) > 0)
+	//fd = open("gnl7_1.txt", O_RDONLY);
+	//while ((a = get_next_line(fd, &line)) > 0)
+	//{
+	//	printf("%d\n", a);
+	//	printf("%s\n", line);
+	//}
+	//printf("%s\n", line);
+
+
+	//fd = 0;
+	fd = open("abc.txt" , O_RDONLY);
+	while ((a = get_next_line(fd, &line) > 0))
 	{
 		printf("%d\n", a);
 		printf("%s\n", line);
 	}
-	//printf("%s\n", line);
+	printf("%d", get_next_line(fd, &line));
+	printf("%d", get_next_line(fd, &line));
+	printf("%d", get_next_line(fd, &line));
+	printf("%d", get_next_line(fd, &line));
 	return (0);
 }
