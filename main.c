24afd81713488375cfbb46111c1b5a620ebded2e
cvsgbhn <kbh123456g@gmail.com>
2019-10-17 00:45:05 +0300

    Made it to compile...

--- main.c
+++ main.c
@@ -6,22 +6,20 @@ int		main(void)
 	int 	fd;
 	int		a;
 
-	//fd = open("gnl7_1.txt", O_RDONLY);
-	//while ((a = get_next_line(fd, &line)) > 0)
+	fd = open("test_file1", O_RDONLY);
+	while ((a = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+	}
+
+
+	//fd = 0;
+	//fd = open("abc" , O_RDONLY);
+	//while ((a = get_next_line(fd, &line) > 0))
 	//{
 	//	printf("%d\n", a);
 	//	printf("%s\n", line);
 	//}
-	//printf("%s\n", line);
-
-
-	fd = 0;
-	fd = open("abc" , O_RDONLY);
-	while ((a = get_next_line(fd, &line) > 0))
-	{
-		printf("%d\n", a);
-		printf("%s\n", line);
-	}
 	//printf("%d", get_next_line(fd, &line));
 	//printf("%d", get_next_line(fd, &line));
 	//printf("%d", get_next_line(fd, &line));
